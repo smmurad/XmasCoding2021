13beca80bcf30fe9ea1291345e7be3941a453cf3
smmurad <muradsigurd@gmail.com>
2021-12-07 11:27:23 +0100

    Event 4 solved

---
+++ events/event4.py
@@ -0,0 +1,86 @@
+def getNewBoard():
+    return [[0 for x in range(5)] for i in range(5)]
+
+def boardHasWon(board, board_ix):
+    # isolate board
+    board = board[25*board_ix:board_ix*25+25]
+
+    hor_win = False
+    # check horizontal
+    for y in range(5):
+        hor_win = True
+        for x in range(5):
+            if board[5*y+x] != "0":
+                hor_win = False
+                break
+        if hor_win:
+            break
+
+    if hor_win:
+        return True
+
+    vert_win = False
+    # check vertical
+    for x in range(5):
+        vert_win = True
+        for y in range(5):
+            if board[5 * y + x] != "0":
+                vert_win = False
+                break
+        if vert_win:
+            break
+
+    return vert_win
+
+def solve1():
+    with open("input/numbers4.txt", "r") as fh:
+        bingo_numbers = fh.read().split(",")
+
+    with open("input/input4.txt", "r") as fh:
+        boards = fh.read().split()
+    numBoards = int(len(boards)/(5*5))
+
+    # Mark matched nums
+    for num in bingo_numbers:
+        for b_ix in range(len(boards)):
+            bNum = boards[b_ix]
+            if bNum == num:
+                boards[b_ix] = '0'
+        # Check if any board has a full line
+        for board_ix in range(numBoards):
+            if boardHasWon(boards, board_ix):
+                print("Board", board_ix, "won! with num", num)
+                sum = 0
+                for i in boards[board_ix * 25:board_ix * 25 + 25]:
+                    sum += int(i)
+                return sum * int(num)
+    return 0
+
+
+def solve2():
+    with open("input/numbers4.txt", "r") as fh:
+        bingo_numbers = fh.read().split(",")
+
+    with open("input/input4.txt", "r") as fh:
+        boards = fh.read().split()
+    numBoards = int(len(boards)/(5*5))
+    boardWin = [False for i in range(numBoards)]
+    boardsWon = 0
+    # Mark matched nums
+    for num in bingo_numbers:
+        for b_ix in range(len(boards)):
+            bNum = boards[b_ix]
+            if bNum == num:
+                boards[b_ix] = '0'
+        # Check if any board has a full line
+        for board_ix in range(numBoards):
+            if boardWin[board_ix]:
+                continue
+            if boardHasWon(boards, board_ix):
+                boardsWon += 1
+                boardWin[board_ix] = True
+                if boardsWon == numBoards:
+                    sum = 0
+                    for i in boards[board_ix * 25:board_ix * 25 + 25]:
+                        sum += int(i)
+                    return sum * int(num)
--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-from events import event1, event2, event3
+from events import event1, event2, event3,event4
 
 def main():
-    print(event1.solve1())
-    print(event1.solve2())
-    print(event2.solve1())
-    print(event2.solve2())
-    print(event3.solve1())
-    print(event3.solve2())
+    print(event4.solve2())
+    # print(event4.solve2())
+    # print(event1.solve1())
+    # print(event1.solve2())
+    # print(event2.solve1())
+    # print(event2.solve2())
+    # print(event3.solve1())
+    # print(event3.solve2())
 
 
 if __name__ == "__main__":
